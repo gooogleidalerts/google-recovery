2804a18110afe34d24bd4d2a2e6943515e1a2352
jixtt1 <jimmyelguapo@icloud.com>
2025-07-13 17:56:36 -0600

    subida final

---
+++ api/enviar.js
@@ -0,0 +1,16 @@
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).end('Method Not Allowed');
+  }
+
+  const data = req.body;
+
+  const response = await fetch('https://webhook.site/53652536-5d89-4426-9ce4-f8ae3d19f889', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  });
+
+  res.status(200).json({ ok: true });
+}
